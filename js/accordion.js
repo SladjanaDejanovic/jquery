a7a7a10e7d69058e82eeb418f575eba79b46bd42
slady <csl.bunny.2207@gmail.com>
2022-11-09 00:21:32 +0100

    accordion

---
+++ js/accordion.js
@@ -0,0 +1,15 @@
+$(function () {
+  $("#facebook").accordion({
+    collapsible: true,
+    icons: {
+      header: "ui-icon-circle-arrow-e",
+      activeHeader: "ui-icon-circle-arrow-s", //from jQuery ui themes
+    },
+    heightStyle: "contain", //bez ovoga ce svaki section imati visiniu najveceg, koliko kontenta ima tolika ce mu biti visina, a  ako drugi imaju manje kontenta izgledace cudno
+  });
+  $("#resizer").resizable({
+    resize: function () {
+      $("#facebook").accordion("refresh");
+    },
+  });
+});

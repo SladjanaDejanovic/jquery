4f166de2555cd667a4ce260ab032406a7f74908b
slady <csl.bunny.2207@gmail.com>
2022-11-09 23:25:05 +0100

    autocomplete input

---
+++ js/autocomplete.js
@@ -0,0 +1,23 @@
+$(function () {
+  $("#city").autocomplete({
+    source: [
+      "London",
+      "Paris",
+      "Belgrade",
+      "Madrid",
+      "Maceio",
+      "Nova Pazova",
+      "Seoul",
+      "Tokyo",
+      "Amsterdam",
+      "Chicago",
+      "Dubrovnik",
+      "Frankfurt",
+      "Glasgow",
+      "Rome",
+      "Florence",
+      "Oslo",
+      "Washington",
+    ],
+  });
+});

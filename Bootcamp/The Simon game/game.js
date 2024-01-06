401c006aa593dd893f40a57ae8c7d08c36a7840a
slady <csl.bunny.2207@gmail.com>
2024-01-06 18:18:42 +0100

    adding sounds

--- Bootcamp/The Simon game/game.js
+++ Bootcamp/The Simon game/game.js
@@ -1,6 +1,13 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
 
+const playSound = function (color) {
+  const audio = $(`#audio-${color}`)[0];
+  audio.currentTime = 0;
+  audio.play();
+  console.log(audio);
+};
+
 const nextSequence = function () {
   const randomNumber = Math.floor(Math.random() * 4);
   const randomChosenColor = buttonColors[randomNumber];
@@ -9,75 +16,20 @@ const nextSequence = function () {
 
   getButton.click(function () {
     const originalColor = $(this).css("background-color");
-    const originalOpacity = $(this).css("opacity");
+    // const originalOpacity = $(this).css("opacity");
 
-    //version 1
-    // Add a flash effect on button click
-    // $(this).animate(
-    //   {
-    //     backgroundColor: "white", // Change background color to white
-    //     opacity: 0.3,
-    //   },
-    //   500,
-    //   function () {
-    //     // Complete the animation and restore the original color and opacity
-    //     $(this).animate(
-    //       {
-    //         backgroundColor: originalColor,
-    //         opacity: originalOpacity,
-    //       },
-    //       500
-    //     );
-    //   }
-    // );
+    playSound(randomChosenColor);
 
-    
-
-    //version 3
-    // $(this)
-    //   .css({
-    //     "box-shadow": "0 0 20px 5px white", // Add a white glow
-    //     // "background-color": "white", // Change background color to white
-    //   })
-    //   .animate(
-    //     {
-    //       opacity: 0.3,
-    //     },
-    //     {
-    //       duration: 600,
-    //       easing: "linear",
-    //       complete: function () {
-    //         // Complete the animation and restore the original color and remove the box-shadow
-    //         $(this).animate(
-    //           {
-    //             opacity: 1,
-    //           },
-    //           {
-    //             duration: 600,
-    //             easing: "linear",
-    //             complete: function () {
-    //               $(this).css({
-    //                 "box-shadow": "none",
-    //                 // "background-color": originalColor,
-    //               });
-    //             },
-    //           }
-    //         );
-    //       },
-    //     }
-    //   );
-
-    //version 4
+    // animate button
     $(this)
       .css({
-        "box-shadow": "0 0 20px 5px #346596", // Add a white glow
-        "background-color": "#346596", // Change background color to white
+        "box-shadow": "0 0 20px 5px #346596",
+        "background-color": "#346596",
       })
       .fadeOut({
         duration: 150,
         easing: "linear",
         done: function () {
-          // Restore the original color before fadeIn
           $(this).css("background-color", originalColor);
         },
       })
@@ -85,12 +37,9 @@ const nextSequence = function () {
         duration: 150,
         easing: "linear",
         done: function () {
-          // Remove the box-shadow after fadeIn is complete
           $(this).css("box-shadow", "none");
         },
       });
   });
-
-  // return randomNumber;
 };
 nextSequence();

60d6796c31f7a8b1e95475196df2fe999910ea57
slady <csl.bunny.2207@gmail.com>
2024-01-07 19:21:20 +0100

    animation and audio

--- Bootcamp/The Simon game/game.js
+++ Bootcamp/The Simon game/game.js
@@ -1,5 +1,7 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
+const userClickedPattern = [];
+// const getButton = $("#" + randomChosenColor);
 
 const playSound = function (color) {
   const audio = $(`#audio-${color}`)[0];
@@ -11,35 +13,45 @@ const playSound = function (color) {
 const nextSequence = function () {
   const randomNumber = Math.floor(Math.random() * 4);
   const randomChosenColor = buttonColors[randomNumber];
+  // const getButton = $("#" + randomChosenColor);
+  // animationAndSound(getButton);
   gamePattern.push(randomChosenColor);
-  const getButton = $("#" + randomChosenColor);
 
-  getButton.click(function () {
-    const originalColor = $(this).css("background-color");
-    // const originalOpacity = $(this).css("opacity");
+  $("#" + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
 
-    playSound(randomChosenColor);
-
-    // animate button
-    $(this)
-      .css({
-        "box-shadow": "0 0 20px 5px #346596",
-        "background-color": "#346596",
-      })
-      .fadeOut({
-        duration: 150,
-        easing: "linear",
-        done: function () {
-          $(this).css("background-color", originalColor);
-        },
-      })
-      .fadeIn({
-        duration: 150,
-        easing: "linear",
-        done: function () {
-          $(this).css("box-shadow", "none");
-        },
-      });
-  });
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
 };
-nextSequence();
+
+// const animationAndSound = function (btn) {
+//   const originalColor = $(this).css("background-color");
+
+//   // play sound
+//   playSound(randomChosenColor);
+
+//   // animate button
+//   $(this)
+//     .css({
+//       "box-shadow": "0 0 20px 5px #346596",
+//       "background-color": "#346596",
+//     })
+//     .fadeOut({
+//       duration: 150,
+//       easing: "linear",
+//       done: function () {
+//         $(this).css("background-color", originalColor);
+//       },
+//     })
+//     .fadeIn({
+//       duration: 150,
+//       easing: "linear",
+//       done: function () {
+//         $(this).css("box-shadow", "none");
+//       },
+//     });
+// };
+
+// nextSequence();

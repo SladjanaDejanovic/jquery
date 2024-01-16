0324b83148acc91533058302f96737e6ac882e84
slady <csl.bunny.2207@gmail.com>
2024-01-16 22:38:11 +0100

    study

--- js/slider.js
+++ js/slider.js
@@ -13,7 +13,7 @@ $(function () {
     min: 0,
     max: 100000,
     values: [0, 10000],
-    step: 1000, //za koliko da se povecava vrednost kada se pomera slajder, nec biti za 1 nego za 1000
+    step: 1000, //za koliko da se povecava vrednost kada se pomera slajder, nece biti za 1 nego za 1000
     slide: function (event, ui) {
       $("#mileageRange").val(
         ui.values[0] + " miles - " + ui.values[1] + " miles"

001f1ee5498a85bd4cac542f5d7210dff9796b2e
slady <csl.bunny.2207@gmail.com>
2024-02-08 21:18:56 +0100

    jquery

--- js/accordion.js
+++ js/accordion.js
@@ -5,7 +5,7 @@ $(function () {
       header: "ui-icon-circle-arrow-e",
       activeHeader: "ui-icon-circle-arrow-s", //from jQuery ui themes
     },
-    heightStyle: "contain", //bez ovoga ce svaki section imati visiniu najveceg, koliko kontenta ima tolika ce mu biti visina, a  ako drugi imaju manje kontenta izgledace cudno
+    heightStyle: "contain", //bez ovoga ce svaki section imati visinu najveceg, koliko kontenta ima tolika ce mu biti visina, a ako drugi imaju manje kontenta izgledace cudno
   });
   $("#resizer").resizable({
     resize: function () {
